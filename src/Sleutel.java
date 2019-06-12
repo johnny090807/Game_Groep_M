e3cc87c3afd60f1a4f7856fde44ac260404a7c5d
john klees <johnnynl@live.nl>
2019-06-12 19:51:15 +0200

    sleutel added.

--- src/Sleutel.java
+++ src/Sleutel.java
@@ -1,2 +1,14 @@
-public class Sleutel {
+public class Sleutel extends BoardObject{
+
+    private int value;
+
+
+
+    public Sleutel(int x, int y, int value){
+        super(x,y);
+
+        this.value = value;
+
+        
+    }
 }
