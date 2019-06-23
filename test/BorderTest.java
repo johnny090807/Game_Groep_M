1fefbc482190426e6d4eff2cde22dba7b00fdc97
johnny090807 <33625576+johnny090807@users.noreply.github.com>
2019-06-23 20:49:22 +0200

    test BorderTest aangepast.

--- test/BorderTest.java
+++ test/BorderTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class BorderTest extends JFrame {
     Board myBoard = new Board();
     @Test
-    public void BorderTest() throws Exception {
+    public void BorderTestY() throws Exception {
         myBoard.startLevel(0);
         Thread.sleep(1000);
 
@@ -31,6 +31,34 @@ public class BorderTest extends JFrame {
         Thread.sleep(1000);
 
         assertEquals(9, myBoard.speler.getPosition().y);
+
+
+    }
+    @Test
+    public void BorderTestX() throws Exception {
+        myBoard.startLevel(0);
+        Thread.sleep(1000);
+
+        setTitle("Game | Collision Test");
+
+        setSize(500,600);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        myBoard.updateHomePanel();
+
+        super.add(myBoard);
+
+        super.setVisible(true);
+
+        for (int i = 0; i < 15; i++){
+            myBoard.beweegSpeler('x', 1);
+            Thread.sleep(1000);
+        }
+
+        Thread.sleep(1000);
+
+        assertEquals(9, myBoard.speler.getPosition().x);
 
 
     }
